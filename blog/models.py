d1956a11157e2a6b691c66d0e8b8557f5c79cde1
Christopher Stevenson <ccstevenson@live.com>
2014-04-30 22:54:20 -0600

    Added some book models.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=300)
+    link = models.CharField(max_length=1000)
+
+    def __unicode__(self):
+        return self.title
+
+
+class ReadBook(Book):
+    date_completed = models.DateField()
+
+
+class CodingPrinciplesBook(ReadBook):
+    pass
+
+
+class PersonalDevelopmentBook(ReadBook):
+    pass
+
+
+class ProfessionalDevelopmentBook(ReadBook):
+    pass
+
+
+class DesignBook(ReadBook):
+    pass
+
+
+class TechnologyBook(ReadBook):
+    pass
+
+
+class ReadingListBook(Book):
+    pass
